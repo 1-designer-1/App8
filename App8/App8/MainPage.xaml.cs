53bc218ab4bb8d995a8934da2643340e023e746f
visual <visual@LAB24-03>
2021-12-07 16:01:59 +0300

    Добавьте файлы проекта.

---
+++ App8/App8/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void data_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            int ag = DateTime.Now.Year - data.Date.Year;
+            if (DateTime.Now.Month < data.Date.Month || (DateTime.Now.Month == data.Date.Month &&
+                DateTime.Now.Month < data.Date.Month))
+            {
+                ag--;
+            }
+            age.Text = "Возраст-" + ag.ToString();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
